6c22e1cb0d06fc2d01f57212ccabc07b83fcfaa9
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-09-08 12:48:45 -0300

    impl: global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello World</h1>
-    </div>
+      <h1>Hello World</h1>
+    </>
   );
 }
